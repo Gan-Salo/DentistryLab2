01637458633d11fce300fb774ea7e3e17db7a557
Георгий Овсянников <ovsyannikov.gosha@list.ru>
2021-09-21 17:17:59 +0700

    Добавьте файлы проекта.

---
+++ DentistryLab2/Cabinet.h
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+#pragma once
+
+struct cabinet
+{	
+	int number;		//Номер кабинета
+	string otdel;	//Отделение, в которое входит кабинет
+	int area;		//Площадь кабинета
+};
+
